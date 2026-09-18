c0c06c315bd4407dab0781ac135ff31f2185c8b7
donnimsipa <85002044+donnimsipa@users.noreply.github.com>
2026-09-18 06:30:43 +0000

    feat: add KernelSU, droidspaces, and GitHub Actions build.

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -79,6 +79,12 @@ void kernel_restart_prepare(char *cmd)
 	device_shutdown();
 }
 
+__weak int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd,
+				     void __user **arg)
+{
+	return 0;
+}
+
 /**
  *	register_reboot_notifier - Register function to be called at reboot time
  *	@nb: Info about notifier function to be called
@@ -287,12 +293,21 @@ static DEFINE_MUTEX(reboot_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
+
+#ifdef CONFIG_KSU 
+	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#endif
 
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
